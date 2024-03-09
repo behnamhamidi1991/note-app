8d3e11aafd01ccdfa7269608909a359128501fa7
Behnam Hamidi <eslp91@hotmail.com>
2024-03-09 15:29:35 +0330

    Pages - Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import Layout from "./layout/Layout";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
